f21acd38869a6768745f22f4fec654dafd74e0ae
codwerk <louisws@outlook.fr>
2021-09-29 16:08:34 +0200

    Gsap

---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+pageTransition = () => {
+  var tl = gsap.timeline();
+
+  tl.to('header', {
+    zIndex: 1
+  });
+
+  tl.to('.page-transition', {
+    duration: 1,
+    height: "100%",
+    top: 0
+  });
+
+  tl.to('.page-transition', {
+    duration: .8,
+    height: "100%",
+    top: "100%",
+    delay: .3
+  });
+
+  tl.set('.page-transition', {
+    top: "-100%"
+  });
+}
+
+mainAnimation = () => {
+   tl = gsap.timeline();
+
+   tl.from('.container h1, .menu-items li, .logo', {
+     duration: 2,
+     y: 30,
+     opacity: 0,
+     stagger: {
+       amount: .4
+     },
+     delay: .8
+   })
+}
+
+pageTransition()
+mainAnimation()
